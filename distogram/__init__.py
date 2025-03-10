ba1691c9aa2c0832a8d21655c87fb91582acc9c6
Romain Picard <romain.picard@softathome.com>
2025-03-10 17:10:09 +0100

    Bump version: 3.0.1 → 3.0.2

--- distogram/__init__.py
+++ distogram/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Romain Picard"""
 __email__ = 'romain.picard@oakbits.com'
-__version__ = '3.0.1'
+__version__ = '3.0.2'
 
 import math
 from bisect import bisect_left
--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'R. Picard'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.0.1'
+release = '3.0.2'
 
 
 # -- General configuration ---------------------------------------------------
